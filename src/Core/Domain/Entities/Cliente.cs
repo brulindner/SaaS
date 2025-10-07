61037601cd73650147268808ff5b9e6fc7916f91
Bruna Lais Lindner <brunalaislindner@outlook.com>
2025-10-07 20:21:39 -0300

    Update API

--- src/Core/Domain/Entities/Cliente.cs
+++ src/Core/Domain/Entities/Cliente.cs
@@ -6,7 +6,7 @@ namespace SaaS.Domain.Entities
     public class Cliente : BaseEntity
 
     {
-        public string Nome { get; set; }  = null!;
+        public string Nome { get; set; }  = null!; //Falta Endereço
         public string Cpf { get; set; }  = null!;
         public string Telefone { get; set; }  = null!;
         public string Email { get; set; }  = null!;
